63ff2dea796c5ba050c645fc7a88207bd029d478
saubuny <saubuny@gmail.com>
2024-01-15 06:33:49 -0500

    Remove about page and remove duplication of navbar via a JS file

---
+++ nav.js
@@ -0,0 +1,18 @@
+document.getElementById("navbar").innerHTML = `
+    <header>
+      <div>
+        <h1>saubuny</h1>
+      </div>
+      <input type="checkbox" id="menu-toggle" />
+      <div id="hamburger-lines">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
+      <nav id="menu">
+        <a href="index.html">home</a>
+        <a href="projects.html">projects</a>
+        <a href="music.html">music</a>
+      </nav>
+    </header>
+`;
