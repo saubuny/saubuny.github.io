16b7d3ea07344458031ee7e87f877eac8a34c850
saubuny <saubuny@gmail.com>
2024-01-16 12:24:34 -0500

    Set up Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./pages/*.html", "./public/js/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
